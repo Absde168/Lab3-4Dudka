06af76e76152ca0376ebd1636bcf876708d9a8e5
B3dRandoms <bdrandoms0@gmail.com>
2023-12-28 11:58:43 +0200

    Initial commit

---
+++ Lab3-4Dudka/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("|    x   |   y    |");
+for (double x = -5; x <= 10; x += 0.2)
+{
+    double y;
+    if (x > 12.5) y = Math.Pow(x, 2) - 3 * x + 2.5 * Math.Pow(x, 2);
+    else if (0 <= x && x <= 1) y = Math.Pow(Math.E, x) + 5 + Math.Cos(0.001 * x);
+    else if (x < 12.5) y = Math.Pow(x, 2);
+    else y = 0;
+    Console.WriteLine($"|{x,8:F2}|{y,8:F2}|");
+}
